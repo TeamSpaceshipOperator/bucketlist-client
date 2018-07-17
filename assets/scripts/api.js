013325ca9aaa027a82d5ac1238bc8f4135c7064f
gofar09 <gordon.d.farrell@gmail.com>
2018-07-17 12:41:07 -0400

    Create files for separation & consolidate api.js -Created ui.js, events.js, api.js -Moved code from restaurant_crud and user_crud to api.js With @bentoks2016 and @reganbp

---
+++ assets/scripts/api.js
@@ -0,0 +1,105 @@
+'use strict'
+
+const config = require('../config.js')
+const store = require('../store')
+
+const createRestaurant = function (data) {
+  return $.ajax({
+    // ajax options go here
+    method: 'POST',
+    url: config.apiUrl + '/restaurants',
+    data: data,
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const getRestaurants = function () {
+  return $.ajax({
+    method: 'GET',
+    url: config.apiUrl + '/restaurants',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const getRestaurant = function (data) {
+  return $.ajax({
+    method: 'GET',
+    url: config.apiUrl + '/restaurants/' + data.id,
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const updateRestaurant = function (data) {
+  // console.log('api says data is ', data)
+  return $.ajax({
+    method: 'PATCH',
+    url: config.apiUrl + '/restaurants/' + data.id,
+    data: {recipe: data},
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const destroyRestaurant = function (data) {
+  return $.ajax({
+    method: 'DELETE',
+    url: config.apiUrl + '/restaurants/' + data.id,
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+// Auth requests below
+const signUp = function (data) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/sign-up',
+    data: data
+  })
+}
+const signIn = function (data) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/sign-in',
+    data: data
+  })
+}
+const changePw = function (data) {
+  return $.ajax({
+    method: 'PATCH',
+    url: config.apiUrl + '/change-password',
+    data: data,
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+const signOut = function () {
+  return $.ajax({
+    method: 'DELETE',
+    url: config.apiUrl + '/sign-out',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  signIn,
+  signUp,
+  signOut,
+  changePw,
+  createRestaurant,
+  getRestaurant,
+  getRestaurants,
+  updateRestaurant,
+  destroyRestaurant
+}
---
+++ assets/scripts/events.js
@@ -0,0 +1,4 @@
+
+const authApi = require('./api')
+const ui = require('./ui')
+const getFormFields = require('../../lib/get-form-fields')
