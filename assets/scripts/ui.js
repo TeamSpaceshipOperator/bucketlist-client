abaa5d150e944747076adeaa93f84848f20971eb
gofar09 <gordon.d.farrell@gmail.com>
2018-07-18 14:43:03 -0400

    Comment out problem code

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -109,7 +109,7 @@ module.exports = {
   createRestaurantSuccess,
   getRestaurantsSuccess,
   destroyRestaurantSuccess,
-  deleteRestaurantFailure,
+  // deleteRestaurantFailure,
   updateRestaurantSuccess,
   updateRestaurantFail,
   viewRestaurantSuccess,
