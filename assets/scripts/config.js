621acc2b8e9ce9d034f7c761be4eb0be1788d603
gofar09 <gordon.d.farrell@gmail.com>
2018-07-18 14:28:41 -0400

    Remove extra / from API url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://young-mountain-70476.herokuapp.com/',
+  production: 'https://young-mountain-70476.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
