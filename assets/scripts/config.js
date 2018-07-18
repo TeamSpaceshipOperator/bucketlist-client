106e8b680dc49b6ad296db7956bd002765a9a11d
gofar09 <gordon.d.farrell@gmail.com>
2018-07-18 14:06:10 -0400

    Add API URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://young-mountain-70476.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
